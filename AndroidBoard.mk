38750bf26e2ffe35990d6755256b3bfa80cca1cb
electimon <electimon@gmail.com>
2022-07-30 01:01:53 +0000

    pstar: Copy touch and related modules to recovery ramdisk
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Ic6205e516461e9a3fe0beecf3b903d43a8a69928

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,4 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+RECOVERY_KMOD_TARGETS += \
+    mmi_relay.ko \
+    mmi_annotate.ko \
+    mmi_info.ko \
+    sensors_class.ko \
+    stmicro_mmi.ko \
+    touchscreen_mmi.ko
+
 -include device/motorola/sm8250-common/AndroidBoardCommon.mk
